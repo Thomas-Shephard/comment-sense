a5eb747fafb146d6d5b46a049e495df36d169e8b
Thomas Shephard <thomas@thomas-shephard.com>
2026-01-22 21:18:17 +0000

    test: refactor test infrastructure

---
+++ tests/CommentSense.Tests/CommentSenseTestBase.cs
@@ -0,0 +1,25 @@
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using NUnit.Framework;
+
+namespace CommentSense.Tests;
+
+public abstract class CommentSenseTestBase
+{
+    protected static async Task VerifyCSenseAsync(string source, bool expectDiagnostic = true)
+    {
+        var tester = new CSharpAnalyzerTest<CommentSenseAnalyzer, NUnitVerifier>
+        {
+            TestCode = source,
+            MarkupOptions = MarkupOptions.UseFirstDescriptor
+        };
+
+        if (expectDiagnostic && !source.Contains("{|") && !source.Contains("[|"))
+            Assert.Fail("expectDiagnostic is true but test code contains no diagnostic markers {| |} or [| |].");
+
+        if (!expectDiagnostic && (source.Contains("{|") || source.Contains("[|")))
+            Assert.Fail("Test code contains diagnostic markers {| |} or [| |] but expectDiagnostic is false.");
+
+        await tester.RunAsync();
+    }
+}
