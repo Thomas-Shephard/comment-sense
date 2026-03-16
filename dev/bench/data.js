c49ef3d6570bec4dd93167bfc86e557206d4426b
github-action-benchmark <github@users.noreply.github.com>
2026-03-16 18:29:40 +0000

    add CommentSense Memory Allocations (benchmarkdotnet) benchmark result for ccd6a13fb8937f305bfb3eed7a3f07fb58283aa0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773595597053,
+  "lastUpdate": 1773685780886,
   "repoUrl": "https://github.com/Thomas-Shephard/comment-sense",
   "entries": {
     "CommentSense Memory Allocations": [
@@ -54,6 +54,60 @@ window.BENCHMARK_DATA = {
             "value": 53165504,
             "unit": "ns",
             "range": "± 5128648.879471378"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas@thomas-shephard.com",
+            "name": "Thomas Shephard",
+            "username": "Thomas-Shephard"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ccd6a13fb8937f305bfb3eed7a3f07fb58283aa0",
+          "message": "feat: implement inheritdoc validation (#109)",
+          "timestamp": "2026-03-16T18:27:59Z",
+          "tree_id": "5ccd637e67da278da3d322f487819878bfea334c",
+          "url": "https://github.com/Thomas-Shephard/comment-sense/commit/ccd6a13fb8937f305bfb3eed7a3f07fb58283aa0"
+        },
+        "date": 1773685780612,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "CommentSense.PerformanceTests.AnalyzerBenchmarks.FullAnalysis(MethodCount: 100, ScanCalledMethods: True, GhostReferenceMode: \"strict\", SimilarityThreshold: 0.8)",
+            "value": 4970947,
+            "unit": "ns",
+            "range": "± 7632263.639695618"
+          },
+          {
+            "name": "CommentSense.PerformanceTests.DogfoodBenchmarks.AnalyzeProject",
+            "value": 19233520,
+            "unit": "ns",
+            "range": "± 58965406.61144239"
+          },
+          {
+            "name": "CommentSense.PerformanceTests.LeakBenchmarks.SimulateLongSession",
+            "value": 5436755,
+            "unit": "ns",
+            "range": "± 4250390.61094563"
+          },
+          {
+            "name": "CommentSense.PerformanceTests.ParallelBenchmarks.ConcurrentAnalysis(FileCount: 100)",
+            "value": 8713972,
+            "unit": "ns",
+            "range": "± 21617187.15524557"
+          },
+          {
+            "name": "CommentSense.PerformanceTests.PathologicalBenchmarks.AnalyzePathologicalDocs(DocSizeMultiplier: 10)",
+            "value": 53098696,
+            "unit": "ns",
+            "range": "± 2874433.9120195983"
           }
         ]
       }
